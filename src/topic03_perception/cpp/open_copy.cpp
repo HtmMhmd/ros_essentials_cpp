0bb9ad5849bf68157a9347a793a18596e0589657
Hatem <yohatem.shadow333@gmail.com>
2024-02-20 10:23:13 +0200

    ros_essentials

--- src/topic03_perception/cpp/open_copy.cpp
+++ src/topic03_perception/cpp/open_copy.cpp
@@ -1,4 +1,6 @@
-#include <highgui.h>
+
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <stdio.h>
 #include "opencv2/opencv.hpp"
@@ -14,7 +16,7 @@ int main( )
        Mat image;
  
        // LOAD image
-       image = imread("/home/riotu/catkin_ws/src/ros_essentials_cpp/src/topic03_perception/images/chess.jpg", CV_LOAD_IMAGE_COLOR);   // Read the file "image.jpg".
+       image = imread("chess.jpg", 0);   // Read the file "image.jpg".
               //This file "image.jpg" should be in the project folder.
               //Else provide full address : "D:/images/image.jpg"
  
@@ -25,7 +27,7 @@ int main( )
        }
  
        //DISPLAY image
-       namedWindow( "window", CV_WINDOW_AUTOSIZE ); // Create a window for display.
+       namedWindow( "window", 0 ); // Create a window for display.
        imshow( "window", image ); // Show our image inside it.
  
        //SAVE image
